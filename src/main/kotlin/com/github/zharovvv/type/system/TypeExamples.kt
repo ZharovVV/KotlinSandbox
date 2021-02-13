4311ca1888c299e375681fc0c7396d3e941122d0
Zharov-VV <VVZharov@sberbank.ru>
2021-02-13 17:54:32 +0500

    Глава 6. Система типов Kotlin. Начало.

---
+++ src/main/kotlin/com/github/zharovvv/type/system/TypeExamples.kt
@@ -0,0 +1,44 @@
+package com.github.zharovvv.type.system
+
+import com.github.zharovvv.classes.User
+
+data class Example(
+    var nullableString: String?,
+    val string: String = "Not Null"
+)
+
+fun main() {
+    val example = Example(null)
+    /**
+     * ?. - оператор безопасного вызова.
+     */
+    example.nullableString?.toUpperCase()   //эквивалентно if (s != null) s.toUpperCase() else null
+    /**
+     * ?: - оператор "Элвис"
+     * foo ?: bar - если foo != null -> foo, иначе bar.
+     */
+    val notNullStringByElvis: String = example.nullableString ?: "DefaultValue if it's null"
+    println(notNullStringByElvis)
+
+    try {
+        val notNullOrException: String =
+            example.nullableString ?: throw IllegalArgumentException("nullableString is null!!")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    println(assertUser(example.nullableString))
+
+    val notNullForCompiler: String = example.nullableString!!
+}
+
+/**
+ * Оператор безопасного приведения типов as?
+ * (Обычный оператор as может бросать исключение ClassCastException)
+ * foo as? Type - если foo is Type -> foo as Type, иначе возвращается null
+ */
+fun assertUser(any: Any?): Boolean {
+    val user: User = any as? User ?: return false
+    println(user)
+    return true
+}
