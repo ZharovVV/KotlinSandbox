0fc790f5d9696a0377ec1b7daf3902f38d6cdf48
Vitaly Zharov <zharov.vitalik@gmail.com>
2021-07-12 15:16:04 +0500

    Добавил в pom.xml поддержку компиляции и сборки Java

--- src/main/kotlin/com/github/zharovvv/Hello.kt
+++ src/main/kotlin/com/github/zharovvv/Hello.kt
@@ -3,7 +3,7 @@ package com.github.zharovvv
 import other.directory.exampleFunction
 
 fun main(args: Array<String>) {
-    exampleFunction("")
+    exampleFunction(" ")
     val list = listOf(1, 2, 3)
     println(list.joinToString(separator = "; ", prefix = "(", postfix = ")"))
 
