b9eccbcc1947c76a9d0b2c9c2fa979006527b290
Zharov Vitaly <zharov.vitalik@gmail.com>
2022-07-14 22:33:46 +0500

    Заметки про лямбда-выражения с получателем типа A.(B) -> C.

--- src/main/kotlin/com/github/zharovvv/lambda/LambdaWithReceivers.kt
+++ src/main/kotlin/com/github/zharovvv/lambda/LambdaWithReceivers.kt
@@ -7,7 +7,7 @@ import com.github.zharovvv.classes.DataClassExample
  * public inline fun <T, R> with(receiver: T, block: T.() -> R): R
  *
  * Лямбда-выражение - это способ определения поведения, похожий на обычную функцию.
- * Лямбда-выражение с получателем - это спосо определения поведения, аналогичный функции-расширению.
+ * Лямбда-выражение с получателем - это способ определения поведения, аналогичный функции-расширению.
  */
 fun alphabet(): String {
 //    val result = StringBuilder()
@@ -45,5 +45,61 @@ fun main() {
         .apply { four = "4etblre" }
         .also { println(it) }
         .let { println(it.four) }
+
+    //------Пример функции с получателем типа T.(R) -> U (в сравнении)
+    "Пупа1".somethingBlockWork_It {
+        it.doSomething(" Лупа1")
+    }
+
+    "Пупа2".somethingBlockWork_This {
+        doSomething(" Лупа2")
+    }
+
+    "Пупа3".somethingBlockWork_SuperMegaThis {
+        println(it) //Пупа3
+        doSomething(" Лупа3")   //Пупа3 Лупа3
+    }
 }
+
+interface SomeInterface<T> {
+    fun doSomething(value: T)
+}
+
+fun <T> T.somethingBlockWork_It(block: (SomeInterface<T>) -> Unit) {
+    val extValue = this
+    val someInterfaceImpl = object : SomeInterface<T> {
+        override fun doSomething(value: T) {
+            println(extValue.toString() + value.toString())
+        }
+    }
+    block.invoke(someInterfaceImpl)
+}
+
+/**
+ * Лямбда-выражение с получателем типа A.() -> C
+ */
+fun <T> T.somethingBlockWork_This(block: SomeInterface<T>.() -> Unit) {
+    val extValue = this
+    val someInterfaceImpl = object : SomeInterface<T> {
+        override fun doSomething(value: T) {
+            println(extValue.toString() + value.toString())
+        }
+    }
+    someInterfaceImpl.block() // are same block.invoke(someInterfaceImpl)
+}
+
+/**
+ * Лямбда-выражение с получателем типа A.(B) -> C
+ */
+fun <T> T.somethingBlockWork_SuperMegaThis(block: SomeInterface<T>.(value: T) -> Unit) {
+    val extValue = this
+    val someInterfaceImpl = object : SomeInterface<T> {
+        override fun doSomething(value: T) {
+            println(extValue.toString() + value.toString())
+        }
+    }
+    someInterfaceImpl.block(extValue) //are same block.invoke(someInterfaceImpl, extValue)
+}
+
+
 
